d40e1915c9039fcd022579eecb113038f06e8e81
Tomas Brejl <tomas.brejl@gmail.com>
2020-06-30 19:50:00 -0700

    first commit

---
+++ Assets/Scripts/Rotater.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Threading;
+using UnityEngine;
+
+public class Rotater : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        System.Random r = new System.Random();
+        int num = r.Next(0, 90);
+        print(num);
+        transform.Rotate(new Vector3(num, num, num) * Time.deltaTime);
+    }
+}
